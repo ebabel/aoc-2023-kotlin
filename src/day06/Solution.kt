67f0d34c47a752ede1546ab1c7db09cb626aaf64
Erik Babel <erik.babel@gmail.com>
2023-12-05 21:42:26 -0800

    day 6, easy!

--- src/day06/Solution.kt
+++ src/day06/Solution.kt
@@ -1,5 +1,7 @@
 package day06
 
+import getLongList
+import productOf
 import readInput
 import solution
 
@@ -7,22 +9,44 @@ fun main() {
     class A; val dir = A().javaClass.packageName
 
     fun part1(input: List<String>): Long {
-        return input.mapIndexed { index, line ->
-            1L
+        val times = input.first().getLongList()
+        val distances = input.last().getLongList()
+        return times.mapIndexed { index, t ->
+            var wins = 0L
+            (1..<t).forEach {t2 ->
+                val didWin = t2 * (t - t2) > distances[index]
+//                println("$t2 beats? ${distances[index]}   $didWin")
+                if (didWin) {
+                    wins++
+                }
+            }
+            wins
 
-        }.sum()
+        }.productOf()
+
     }
 
     fun part2(input: List<String>): Long {
-        return input.mapIndexed { index, line ->
-            1L
-        }.sum()
+        val time = input.first().substringAfter(" ").replace(" ", "").toLong()
+        val distance = input.last().substringAfter(" ").replace(" ", "").toLong()
+        return time.let {  t ->
+            var wins = 0L
+            (1..<t).forEach {t2 ->
+                val didWin = t2 * (t - t2) > distance
+//                println("$t2 beats? ${distances[index]}   $didWin")
+                if (didWin) {
+                    wins++
+                }
+            }
+            wins
+
+        }
     }
     val testInput = readInput("$dir/test_input")
 
-    solution("Part1 test: ", 1L, true) { part1(testInput) }
-//    solution("Part2 test: ", 1L, true) { part2(testInput) }
+    solution("Part1 test: ", 288L, true) { part1(testInput) }
+    solution("Part2 test: ", 71503L, true) { part2(testInput) }
     val input = readInput("$dir/input")
-    solution("Part1 solution: ") { part1(input) }
-//    solution("Part2 solution: ") { part2(input) }
+//    solution("Part1 solution: ") { part1(input) }
+    solution("Part2 solution: ") { part2(input) }
 }
